9b297b9af10dda0123cb0f9295fa4ea8a1d7e82e
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-21 16:34:06 +0200

    blocco nella creazione del file js

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+*/
+
+/*
+
+1- generare le caselle con un numero massimo
+2- disporle random
+2- inserire il numero all'interno 
+3- al 
+
+*/
+
+function inserimento() {
+  let cs = document.createElement("div");
+  btn.innerHTML = "scratch";
+  document.getElementById("pulsante").appendChild(btn);
+}
+</script>
